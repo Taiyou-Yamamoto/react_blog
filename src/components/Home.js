449716da40b1feaec4deb8606c8f69010ae7c2f0
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-07-24 21:47:53 +0900

    データ取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,33 @@
-import React from "react";
-import "./Home.css";
+import React, { useEffect } from 'react';
+import './Home.css';
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Home = () => {
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      console.log(data);
+      console.log(data.docs);
+      console.log(data.docs.map((doc) => ({ doc })));
+      console.log(data.docs.map((doc) => ({ ...doc.data().author.username })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
       <div className="postContetnts">
         <div className="postHeader">
           <h1>タイトル</h1>
         </div>
-      </div>
-      <div className="postTextContainer">
-        今はReactの学習中です。これから頑張ってReactエンジニアとして頑張っていきたいと思います。
-      </div>
-      <div className="nameAndDeleteButton">
-        <h3>@Yamamoto</h3>
+        <div className="postTextContainer">
+          今はReactの学習中です。これから頑張ってReactエンジニアとして頑張っていきたいと思います。sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Yamamoto</h3>
+          <button>削除</button>
+        </div>
       </div>
     </div>
   );
